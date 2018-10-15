7951c3acb4a31099b1148aa4119413d4b7f00337
Unknown <sam.van.bogaert@hotmail.com>
2018-10-15 12:07:23 +0200

    Added app to git

---
+++ src/app/Risk/src/pages/attack-phase2/attack-phase2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AttackPhase2Page } from './attack-phase2';
+
+@NgModule({
+  declarations: [
+    AttackPhase2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(AttackPhase2Page),
+  ],
+})
+export class AttackPhase2PageModule {}
