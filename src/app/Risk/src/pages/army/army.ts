2774c606fa033411a63620e39a65f65db040e5eb
Unknown <inias--@hotmail.com>
2018-12-02 02:03:02 +0100

    Army page finished

--- src/app/Risk/src/pages/army/army.ts
+++ src/app/Risk/src/pages/army/army.ts
@@ -17,9 +17,10 @@ import { NgProgressService } from 'ng2-progressbar';
 })
 export class ArmyPage {
 
+  public TroopsSend: number;
   PlayerInfo: Player;//[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private service: ApiService , private alertC:AlertController , private pService: NgProgressService  ) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private service: ApiService, private alertC: AlertController, private pService: NgProgressService) {
   }
 
 
@@ -32,32 +33,38 @@ export class ArmyPage {
 
   test() {
     console.log(this.PlayerInfo);
+    console.log(this.TroopsSend);
   }
 
-  
+
 
   MoveTroops(_troops: number) {
-    this.Alert("Troops are now walking 2 your location!");
-    this.pService.start();
-    setTimeout(() => {
-      this.service.PutInfo(this.service.GetYourId(),
-        {
-          playerId: this.service.GetYourId(),
-          playerTroops: `${(this.PlayerInfo.playerTroops) + _troops}`,
-          playerReserveTroops: `${(this.PlayerInfo.playerReserveTroops) - _troops}`
-        })
-        .subscribe(data => this.PlayerInfo = data);
-      //alert("Troops have arrived");
-      this.Alert("Troops has arrived!");
-      this.pService.done();
-    }, 50000);
+    _troops = Number(this.TroopsSend);
+    if (_troops != null && _troops != 0 && _troops > 0) {
+      this.Alert("Troops are now walking 2 your location!");
+      this.pService.start();
+      setTimeout(() => {
+        this.service.PutInfo(this.service.GetYourId(),
+          {
+            playerId: this.service.GetYourId(),
+            playerTroops: `${(this.PlayerInfo.playerTroops) + _troops}`,
+            playerReserveTroops: `${(this.PlayerInfo.playerReserveTroops) - _troops}`
+          })
+          .subscribe(data => this.PlayerInfo = data);
+        //alert("Troops have arrived");
+        this.Alert("Troops has arrived!");
+        this.pService.done();
+      }, 10000);
+    }
+    else
+      this.Alert("Fill in how many troops!");
   }
 
 
 
-  Alert(message:string){
+  Alert(message: string) {
     let Alertm = this.alertC.create({
-      message:`${message}`
+      message: `${message}`
     });
     Alertm.present();
   }
