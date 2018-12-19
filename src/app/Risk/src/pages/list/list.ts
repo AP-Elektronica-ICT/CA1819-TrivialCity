5cf06368e673b3646de11382b2c35f939fb3d2a7
Unknown <inias--@hotmail.com>
2018-12-19 16:31:35 +0100

    working test alert with signalR

--- src/app/Risk/src/pages/list/list.ts
+++ src/app/Risk/src/pages/list/list.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { ApiService, Player } from '../../services/api.service';
 import { AuthService } from '../../services/auth.service';
 import { Observable } from 'rxjs';
@@ -23,7 +23,7 @@ export class ListPage implements OnInit {
   PlayerData: Player[] = [];
   hubConnection: HubConnection;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams ,  private service: ApiService ,public auth: AuthService ) {
+  constructor(public navCtrl: NavController, public navParams: NavParams ,  private service: ApiService ,public auth: AuthService ,private alertC: AlertController ) {
     // If we navigated to this page, we will have an item available as a nav param
     
   }
@@ -41,6 +41,7 @@ export class ListPage implements OnInit {
 
     this.hubConnection.on("Send",data => {
       console.log(data);
+      this.Alert(data);
     });
 
   }
@@ -53,8 +54,16 @@ Check2(){
   this.service.GetPlayers().subscribe(data => this.PlayerData = data);
   console.log(this.PlayerData);
   //this.service.testPost();
-
+  this.hubConnection.invoke("Send", "Under Attack!");
   
+}
+
+
+Alert(message: string) {
+  let Alertm = this.alertC.create({
+    message: `${message}`,
+  });
+  Alertm.present();
 }
 
 
