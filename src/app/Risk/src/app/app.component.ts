49d7065935a4a3aeb93b231767aa5c36def66602
Kevin Van de Vyver <kevinvandevyver1@gmail.com>
2019-01-06 18:57:31 +0100

    Shop made, might need some layout tweeks

--- src/app/Risk/src/app/app.component.ts
+++ src/app/Risk/src/app/app.component.ts
@@ -16,7 +16,6 @@ import Auth0Cordova from '@auth0/cordova';
 import { TeamViewPage } from '../pages/teamView/teamView';
 import { SignalrService } from '../services/signalR.service';
 import { ShopPage } from '../pages/shop/shop';
-import { DebugPage } from '../pages/debug/debug';
 
 
 @Component({
@@ -63,7 +62,6 @@ export class MyApp implements OnInit{
       { title: 'Leaderboards', component: ListPage },
       { title: 'Shop', component: ShopPage },
       { title: 'Register', component: RegisterPage },
-      { title: 'Debug', component: DebugPage}
     ];
 
   }
