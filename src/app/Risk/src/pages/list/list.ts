f1d3a04746efb50297e9fdfa4f54b2191291e1f2
Kevin Van de Vyver <kevinvandevyver1@gmail.com>
2019-01-10 12:05:58 +0100

    Leaderboards added

--- src/app/Risk/src/pages/list/list.ts
+++ src/app/Risk/src/pages/list/list.ts
@@ -17,11 +17,45 @@ import { TeamPage } from '../team/team';
 })
 export class ListPage {
 
+  players: Player[] = [];
+  playerLevels: number[] = [];
+  src: string[] = [];
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private service: ApiService, public auth: AuthService, private alertC: AlertController, private SingalRservice: SignalrService) {
 
   }
 
   ionViewDidLoad() {
+    console.log("'ionViewDidLoad ListPage'");
+    this.service.GetPlayers().subscribe(data => {
+      this.players = data;
+      this.SortPlayers();
+      this.RankChecker();
+    });
+  }
+
+  SortPlayers() {
+    this.players.sort((a, b) => b.playerLevel - a.playerLevel);
+    console.log("players sorted by level: " + this.players);
+  }
+
+
+  RankChecker() {
+
+    this.players.forEach(Player => {
+      if (Player.playerLevel < 5) { this.src.push('../../assets/imgs/ranks/private.png')}
+      else if (Player.playerLevel >= 50) { this.src.push('../../assets/imgs/ranks/sergeant_major_of_the_army.png')}
+      else if (Player.playerLevel >= 45) { this.src.push('../../assets/imgs/ranks/command_sergeant_major.png')}
+      else if (Player.playerLevel >= 40) { this.src.push('../../assets/imgs/ranks/sergeant_major.png')}
+      else if (Player.playerLevel >= 35) { this.src.push('../../assets/imgs/ranks/first_sergeant.png')}
+      else if (Player.playerLevel >= 30) { this.src.push('../../assets/imgs/ranks/master_sergeant.png')}
+      else if (Player.playerLevel >= 25) { this.src.push('../../assets/imgs/ranks/sergeant_first_class.png')}
+      else if (Player.playerLevel >= 20) { this.src.push('../../assets/imgs/ranks/staff_sergeant.png')}
+      else if (Player.playerLevel >= 15) { this.src.push('../../assets/imgs/ranks/sergeant.png')}
+      else if (Player.playerLevel >= 10) { this.src.push('../../assets/imgs/ranks/corporal.png')}
+      else if (Player.playerLevel >= 5) { this.src.push('../../assets/imgs/ranks/private_first_class.png')}
+    });
+
 
   }
 
